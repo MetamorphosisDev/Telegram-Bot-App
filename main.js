63a24ecd09049b96e5976fb4a991e978499a566b
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-06-25 21:22:02 +0700

    Update README.md

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ const telegramClient = new TelegramBot(process.env.TOKEN_BOT, option_System)
 
 telegramClient.on("message", (callback) => {
     const id = callback.from.id
-
     telegramClient.sendMessage(id, callback.text)
 })
 
-const Sayhi = /^halo$/
-telegramClient.onText(Sayhi, (callback) => {
-    telegramClient.sendMessage(callback.from.id, "Halo juga")
-})
